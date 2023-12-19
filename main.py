9b41b009ca47ed36c17cc9974cc6c796d95f16cf
Levi Szamek <levi@szamek.de>
2023-12-19 13:45:01 +0100

    feat(dnbr): add calculation and plotting of dnbr

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from rasterio.plot import plotting_extent
 
-from utils import get_paths_to_bands, combine_tifs, calculate_nbr, plot_nbr
+from utils import get_paths_to_bands, combine_tifs, calculate_nbr, plot_nbr, plot_rgb, plot_satellite_imagery, plot_dnbr
 
 all_landsat_bands_path = get_paths_to_bands("bc_burn", [5, 6, 7])
 all_landsat_bands_path.sort()
@@ -13,3 +13,7 @@ extent_landsat = plotting_extent(
 landsat_postfire_nbr = calculate_nbr(landsat_post_fire)
 
 plot_nbr(landsat_postfire_nbr, extent_landsat)
+
+landsat_postfire_nbr -= 100
+
+plot_dnbr(landsat_postfire_nbr, extent_landsat)
--- utils.py
+++ utils.py
@@ -1,11 +1,21 @@
 import os
 import re
+from glob import glob
 
+import numpy as np
 import xarray as xr
 import rioxarray as rxr
-
+import yaml
 import matplotlib.pyplot as plt
+import earthpy.spatial as es
 import earthpy.plot as ep
+from matplotlib.colors import ListedColormap
+from numpy import ma
+
+
+def get_from_config(key: str):
+    with open('config.yaml', 'r') as config_file:
+        return yaml.safe_load(config_file)[key]
 
 def combine_tifs(tif_list):
     """
@@ -45,14 +55,18 @@ def get_paths_to_bands(path_to_directory: str, bands: list):
                 Returns None if no matching files are found.
 
     Example:
-    >>> get_paths_to_bands('data', [3, 4, 5])
+    get_paths_to_bands('data', [3, 4, 5])
     ['/path/to/data/example_B3.TIF', '/path/to/data/example_B4.TIF', '/path/to/data/example_B5.TIF']
 
     """
-
-    bands_pattern = ','.join(map(str, bands))
+    if bands is None or len(bands) == 0:
+        print("No bands specified.")
+        return None
+    elif len(bands) == 1:
+        bands_pattern = str(bands[0])
+    else:
+        bands_pattern = ','.join(map(str, bands))
     current_directory = os.path.dirname(__file__)
-    print("Current Directory:", current_directory)
 
     folder_path = os.path.join(current_directory, path_to_directory)
 
@@ -78,7 +92,7 @@ def plot_rgb(image_path, figsize=(10, 10)):
     - None
 
     Example:
-    >>> plot_rgb('data/example.tif', figsize=(10, 10))
+     plot_rgb('data/example.tif', figsize=(10, 10))
 
     """
 
@@ -87,8 +101,23 @@ def plot_rgb(image_path, figsize=(10, 10)):
 
 
 def calculate_nbr(bands):
-    return (bands[0] - bands[2]) / (bands[0] + bands[2])
+    """
+    Calculate the Normalized Burn Ratio (NBR) using input bands.
 
+    The Normalized Burn Ratio is computed as (NIR - SWIR) / (NIR + SWIR),
+    where NIR is the near-infrared band (bands[0]) and SWIR is the shortwave
+    infrared band (bands[2]).
+
+    Parameters:
+    - bands (DataArray): A DataArray containing the spectral bands.
+                             The order is assumed to be [NIR, ..., SWIR, ...].
+
+    Returns:
+    - DataArray: A DataArray with the computed Normalized Burn Ratio (NBR) value.
+
+    """
+
+    return (bands[0] - bands[2]) / (bands[0] + bands[2])
 
 def plot_nbr(bands, extent):
     fig, ax = plt.subplots(figsize=(12, 6))
@@ -102,3 +131,96 @@ def plot_nbr(bands, extent):
                   title="Landsat derived Normalized Burn Ratio\n 23 July 2016 \n Post Cold Springs Fire")
 
     plt.show()
+
+def plot_satellite_imagery(path_to_directory, bands):
+
+    #
+    # stack_band_paths = get_paths_to_bands(path_to_directory, bands)
+    # stack_band_paths.sort()
+    #
+    # arr_st, meta = es.stack(stack_band_paths, nodata=-9999)
+    #
+    # fig, ax = plt.subplots(figsize=(12, 12))
+    #
+    # ep.plot_rgb(arr_st,
+    #             title='RGB Satellite Image',
+    #             stretch=True,  # Apply histogram stretch to improve contrast
+    #             str_clip=0.5,  # Adjust this parameter to control the stretch intensity
+    #             figsize=(10, 10))
+    # plt.show()
+
+    red_band = get_paths_to_bands(path_to_directory, [bands[0]])[0]
+    green_band = get_paths_to_bands(path_to_directory, [bands[1]])[0]
+    blue_band = get_paths_to_bands(path_to_directory, [bands[2]])[0]
+
+    # Stack the three bands
+    arr_st, meta = es.stack([red_band, green_band, blue_band], nodata=-9999)
+
+    fig, ax = plt.subplots(figsize=(12, 12))
+
+    ep.plot_rgb(arr_st,
+                title='RGB Satellite Image',
+                stretch=True,  # Apply histogram stretch to improve contrast
+                str_clip=0.5,  # Adjust this parameter to control the stretch intensity
+                figsize=(10, 10))
+    plt.show()
+
+def calculate_dnbr(pre_fire_nbr, post_fire_nbr):
+    """
+    Calculate the differenced normalized burn ratio (dNBR) using input bands.
+
+    The differenced normalized burn ratio is computed as (pre-fire NBR - post-fire NBR).
+
+    Parameters:
+    - pre_fire_nbr (DataArray): A DataArray containing the pre-fire spectral bands.
+    - post_fire_nbr (DataArray): A DataArray containing the post-fire spectral bands.
+
+    Returns:
+    - DataArray: A DataArray with the computed differenced normalized burn ratio (dNBR) value.
+
+    """
+
+    return pre_fire_nbr - post_fire_nbr
+
+def plot_dnbr(dnbr, extent):
+    dnbr_cat_names = get_from_config("dnbr_cat_names")
+
+    nbr_colors = get_from_config("nbr_colors")
+    nbr_cmap = ListedColormap(nbr_colors)
+
+    # Define dNBR classification bins
+    # reclassify raster https://www.earthdatascience.org/courses/use-data-open-source-python/intro-raster-data-python/raster-data-processing/classify-plot-raster-data-in-python/
+    dnbr_class_bins = get_from_config("dnbr_class_bins")
+
+    #dnbr_landsat_class = np.digitize(dnbr, dnbr_class_bins)
+
+    dnbr_landsat_class = xr.apply_ufunc(np.digitize,
+                                        dnbr,
+                                        dnbr_class_bins)
+    # Plot the data with a custom legend
+    dnbr_landsat_class_plot = ma.masked_array(
+        dnbr_landsat_class.values, dnbr_landsat_class.isnull())
+
+    fig, ax = plt.subplots(figsize=(10, 8))
+
+    classes = np.unique(dnbr_landsat_class_plot)
+    classes = classes.tolist()[:5]
+
+    ep.plot_bands(dnbr_landsat_class_plot,
+                  cmap=nbr_cmap,
+                  vmin=1,
+                  vmax=5,
+                  title="Landsat dNBR - Cold Spring Fire Site \n June 22, 2016 - July 24, 2016",
+                  cbar=False,
+                  scale=False,
+                  extent=extent,
+                  ax=ax)
+
+    ep.draw_legend(im_ax=ax.get_images()[0],
+                   classes=classes,
+                   titles=dnbr_cat_names)
+
+    plt.show()
+
+
+
